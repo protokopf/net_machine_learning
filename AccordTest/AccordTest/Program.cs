b7b0d801d7bac8f69af41492816d3fa6d4dc9b2b
Konstantin Kovalenko <protokopf@gmail.com>
2018-06-21 23:17:02 +0300

    - Added NN implementation

--- AccordTest/AccordTest/Program.cs
+++ AccordTest/AccordTest/Program.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Accord.Controls;
+using Accord.IO;
+using Accord.Math;
+using Accord.Statistics.Distributions.Univariate;
+using Accord.MachineLearning.Bayes;
+using System.Data;
+using Accord.Neuro;
+using Accord.Neuro.Learning;
 
 namespace AccordTest
 {
@@ -10,6 +18,55 @@ namespace AccordTest
     {
         static void Main(string[] args)
         {
+            // Read the Excel worksheet into a DataTable
+            DataTable table = new ExcelReader("examples.xls").GetWorksheet("Classification - Yin Yang");
+
+            // Convert the DataTable to input and output vectors
+            double[][] inputs = table.ToJagged<double>("X", "Y");
+            int[] outputs = table.Columns["G"].ToArray<int>();
+
+            // Since we would like to learn binary outputs in the form
+            // [-1,+1], we can use a bipolar sigmoid activation function
+            IActivationFunction function = new BipolarSigmoidFunction();
+
+            // In our problem, we have 2 inputs (x, y pairs), and we will 
+            // be creating a network with 5 hidden neurons and 1 output:
+            //
+            var network = new ActivationNetwork(function,
+                inputsCount: 2, neuronsCount: new[] { 10, 10,  1 });
+
+            // Create a Levenberg-Marquardt algorithm
+            var teacher = new LevenbergMarquardtLearning(network, true)
+            {
+                UseRegularization = true
+            };
+
+            // Because the network is expecting multiple outputs,
+            // we have to convert our single variable into arrays
+            //
+            double[][] y = outputs.ToDouble().ToArray();
+
+            // Iterate until stop criteria is met
+            double error = double.PositiveInfinity;
+            double previous;
+
+            do
+            {
+                previous = error;
+
+                // Compute one learning iteration
+                error = teacher.RunEpoch(inputs, y);
+
+            } while (Math.Abs(previous - error) < 0.05);
+
+
+            // Classify the samples using the model
+            int[] answers = inputs.Apply(network.Compute).GetColumn(0).Apply(Math.Sign);
+
+            // Plot the results
+            ScatterplotBox.Show("Expected results", inputs, outputs);
+            ScatterplotBox.Show("Network results", inputs, answers)
+            .Hold();
         }
     }
 }
